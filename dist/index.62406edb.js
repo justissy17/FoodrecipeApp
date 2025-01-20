6b7e11e19c6da5e6186fef14145c970c9e014089
Uka Israel <isrealuka@gmail.com>
2025-01-20 04:02:08 +0100

    working on the search section of the app

---
+++ dist/index.62406edb.js
@@ -0,0 +1,34 @@
+const recipeContainer = document.querySelector('.recipe');
+const timeout = function(s) {
+    return new Promise(function(_, reject) {
+        setTimeout(function() {
+            reject(new Error(`Request took too long! Timeout after ${s} second`));
+        }, s * 1000);
+    });
+};
+///////////////////////////////////////
+const showRecipe = async function() {
+    try {
+        const res = await fetch(`https://forkify-api.jonas.io/api/v2/recipes/664c8f193e7aa067e94e8297`);
+        const data = await res.json();
+        console.log(res, data);
+        if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+        let { recipe } = data.data;
+        recipe = {
+            id: recipe.id,
+            title: recipe.title,
+            publisher: recipe.publisher,
+            sourceUrl: recipe.source_url,
+            image: recipe.image_url,
+            servings: recipe.servings,
+            cookingTime: recipe.cooking_time,
+            ingredients: recipe.ingredients
+        };
+        console.log(recipe);
+    } catch (err) {
+        alert(err);
+    }
+};
+showRecipe();
+
+//# sourceMappingURL=index.62406edb.js.map
