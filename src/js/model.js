7875944aaed411f9fc71f26c45cfb7adc1ab26d7
Uka Israel <isrealuka@gmail.com>
2025-01-22 13:03:36 +0100

    still working

---
+++ src/js/model.js
@@ -0,0 +1,35 @@
+import { async } from 'regenerator-runtime/runtime';
+
+export const state = {
+  recipe: {},
+};
+
+export const loadRecipe = async function (id) {
+  try {
+    console.log(id);
+    const res = await fetch(
+      `https://forkify-api.jonas.io/api/v2/recipes/${id}`
+    );
+    const data = await res.json();
+    console.log(res, data);
+
+    if (!res.ok) {
+      throw new Error(`${data.message} (${res.status})`);
+    }
+    let { recipe } = data.data;
+
+    state.recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+    console.log(state.recipe);
+  } catch (error) {
+    alert(error);
+  }
+};
